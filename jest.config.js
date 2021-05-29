39823e417070864a69e1b6da3f235702bcfd3cbb
Herbert Fonzar <herbertfonzar@gmail.com>
2021-05-29 00:41:26 -0300

    add initial config, intial controller and inital test

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const { resolve } = require('path')
+const root = resolve(__dirname)
+module.exports = {
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/src/**/*.test.js'],
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1'
+  }
+}
