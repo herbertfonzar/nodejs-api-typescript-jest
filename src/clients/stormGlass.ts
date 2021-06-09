62f67009f2ff5ceee80c2f7ab99e3df530477943
Herbert Fonzar <herbertfonzar@gmail.com>
2021-06-09 01:06:35 -0300

    add client stormGlass

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,6 @@
-import { InternalError } from "@src/util/errors/internal-errors";
-import { AxiosStatic } from "axios";
+import { InternalError } from "@src/util/errors/internal-errors"
+import * as HTTPUtil from '@src/util/request'
+import config, { IConfig } from 'config'
 
 export interface StormGlassPointSource {
     [key: string]: number;
@@ -45,25 +46,27 @@ export class StormGlassResponseError extends InternalError{
     }
 }
 
+const stormGlassResourceConfig: IConfig = config.get('App.resources.StormGlass')
+
 export class StormGlass {
     readonly stormGlassAPIParams = 'smellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,winDirection,windSpeed';
     readonly stormGlassAPISource = 'noaa';
 
-    constructor(protected request: AxiosStatic) { }
+    constructor(protected request = new HTTPUtil.Request()) { }
 
     public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
         try{
             const response = await this.request.get<StormGlassForecastResponse>(
-                `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`,
+                `${stormGlassResourceConfig.get('apiUrl')}/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`,
                 {
                     headers: {
-                        Authorization: 'fake-token'
+                        Authorization: stormGlassResourceConfig.get('apiToken')
                     }
                 }
             )
             return this.normalizedResponse(response.data);
         } catch (err) {
-            if (err.response && err.response.status) {
+            if (HTTPUtil.Request.isRequestError(err)) {
                 throw new StormGlassResponseError(`Error ${JSON.stringify(err.response.data)} Code: ${err.response.status}`)
             }
             throw new ClientRequestError(err.message)            
