1843d24a23ef942e42340e2ca2af224de1ac4c95
Herbert Fonzar <herbertfonzar@gmail.com>
2021-06-13 18:39:52 -0300

    commit

--- src/controller/forecast.ts
+++ src/controller/forecast.ts
@@ -1,14 +1,14 @@
-import { Controller, Get } from '@overnightjs/core';
-import { Beach } from '@src/models/beach';
-import { Forecast } from '@src/services/forecast';
-import { Request, Response } from 'express';
+import { Controller, Get } from '@overnightjs/core'
+import { Beach } from '@src/models/beach'
+import { Forecast } from '@src/services/forecast'
+import { Request, Response } from 'express'
 
 const forecast = new Forecast()
 
 @Controller('forecast')
 export class ForecastController {
   @Get('/')
-  public async getForecastLoggedUser(_: Request, res: Response): Promise<void> {
+  public async getForecastLoggedUser (_: Request, res: Response): Promise<void> {
     try {
       const beaches = await Beach.find()
       const forecastData = await forecast.processForecastForBeaches(beaches)
@@ -16,6 +16,5 @@ export class ForecastController {
     } catch (error) {
       res.status(500).send({ error: 'Something went wrong' })
     }
-
   }
 }
